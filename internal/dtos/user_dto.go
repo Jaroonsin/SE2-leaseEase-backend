1bee22e8fc15e6ddb91e354f7a27207de7c3175c
tnsp <wavet888@gmail.com>
2025-01-26 13:59:21 +0700

    test commit

--- internal/dtos/user_dto.go
+++ internal/dtos/user_dto.go
@@ -8,3 +8,12 @@ type RegisterDTO struct {
 	Password string `json:"password"` // user's password
 	Role     string `json:"role"`     // role: lessor, lessee
 }
+
+type TestDTO struct {
+	ID       uint   `json:"id"`       // student id
+	Name     string `json:"name"`     // user's first name
+	Address  string `json:"address"`  // user's address
+	Email    string `json:"email"`    // user's email
+	Password string `json:"password"` // user's password
+	Role     string `json:"role"`     // role: lessor, lessee
+}
