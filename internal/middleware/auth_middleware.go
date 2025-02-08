38b53c3e5810920fe23ca074becee1c9b3bfcf4f
bblueberries <tata10433@gmail.com>
2025-02-08 23:51:19 +0700

    fix: cfg naming

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -7,7 +7,7 @@ import (
 	jwtware "github.com/gofiber/jwt/v3"
 )
 
-func AuthorizationUserToken(cfg *config.Config) fiber.Handler {
+func AuthorizationUserToken(cfg *config.DBConfig) fiber.Handler {
     return jwtware.New(jwtware.Config{
         SigningKey:   []byte(cfg.JWTSecret),
         ErrorHandler: AuthError, 
