b7324a5e7a50465348f7bfb9221f7ad8d9c25d2b
bblueberries <tata10433@gmail.com>
2025-02-08 22:19:40 +0700

    add: response utils

---
+++ utils/response_utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import "github.com/gofiber/fiber/v2"
+
+type Response struct {
+	StatusCode int         `json:"status_code"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data,omitempty"` 
+}
+
+func SuccessResponse(c *fiber.Ctx, statusCode int, message string, data interface{}) error {
+	return c.Status(statusCode).JSON(Response{
+		StatusCode: statusCode,
+		Message:    message,
+		Data:       data,
+	})
+}
+
+func ErrorResponse(c *fiber.Ctx, statusCode int, message string) error {
+	return c.Status(statusCode).JSON(Response{
+		StatusCode: statusCode,
+		Message:    message,
+	})
+}
