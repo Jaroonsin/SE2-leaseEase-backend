1dec51b1db0c23d3dbbfb2d3f97bf43e72eab2c7
bblueberries <tata10433@gmail.com>
2025-02-15 22:31:36 +0700

    add: jwtDTO,add role in claim

--- internal/dtos/auth_dto.go
+++ internal/dtos/auth_dto.go
@@ -2,15 +2,20 @@ package dtos
 
 // @Description RegisterDTO represents a request for user registration.
 type RegisterDTO struct {
-	Email    string `json:"email" example:"john.doe@example.com"`       // user's email
-	Password string `json:"password" example:"strongPassword123"`       // user's password
-	Name     string `json:"name" example:"John"`                        // user's first name
-	Address  string `json:"address" example:"1234 Main St, Springfield"`// user's address
-	Role     string `json:"role" example:"lessee"`                     // role: lessor, lessee
+	Email    string `json:"email" example:"john.doe@example.com"`        // user's email
+	Password string `json:"password" example:"strongPassword123"`        // user's password
+	Name     string `json:"name" example:"John"`                         // user's first name
+	Address  string `json:"address" example:"1234 Main St, Springfield"` // user's address
+	Role     string `json:"role" example:"lessee"`                       // role: lessor, lessee
 }
 
 // @Description LoginDTO represents a request for user login.
 type LoginDTO struct {
 	Email    string `json:"email" example:"john.doe@example.com"` // user's email
 	Password string `json:"password" example:"strongPassword123"` // user's password
+}
+
+type JWTDTO struct {
+	UserID int    `json:"user_id" example:"1"`
+	Role   string `json:"role" example:"lessee" `
 }
--- utils/jwt_utils.go
+++ utils/jwt_utils.go
@@ -4,20 +4,22 @@ import (
 	"time"
 
 	"LeaseEase/config"
+	"LeaseEase/internal/dtos"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 // GenerateJWT generates a JWT token for a user with a given user ID
-func GenerateJWT(userID uint) (string, error) {
+func GenerateJWT(user dtos.JWTDTO) (string, error) {
 	// Define the secret key (should come from environment variables for security)
 	secretKey := config.LoadConfig().JWTApiSecret
 
 	// Define token claims
 	claims := jwt.MapClaims{
-		"user_id": userID,
+		"user_id": user.UserID,
 		"exp":     time.Now().Add(time.Hour * 3).Unix(), // Token expiration: 24 hours
-		"iat":     time.Now().Unix(),                     // Issued at time
+		"iat":     time.Now().Unix(),                    // Issued at time
+		"role":    user.Role,
 	}
 
 	// Create the token
