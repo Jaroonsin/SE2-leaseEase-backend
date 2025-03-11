f8e94050c56a7fd0fcf8151d0e05763a2b73070f
bblueberries <tata10433@gmail.com>
2025-03-11 10:27:51 +0700

    fix: include lessee name,image to getReservByProp

--- internal/services/lessor.go
+++ internal/services/lessor.go
@@ -6,5 +6,5 @@ type LessorService interface {
 	AcceptReservation(reservationID uint, lessorID uint) (*dtos.ReservationResponseDTO, error)
 	DeclineReservation(reservationID uint, lessorID uint) (*dtos.ReservationResponseDTO, error)
 
-	GetReservationsByPropertyID(propertyID uint, page int, pageSize int) ([]dtos.GetReservationDTO, error)
+	GetReservationsByPropertyID(propertyID uint, page int, pageSize int) ([]dtos.GetPropReservationDTO, error)
 }
